7c8db0798094338238175ff9d68ec30995e36b4b
Viggo Petersen <vig56@hotmail.com>
2018-10-12 18:28:20 +0200

    Add minor navigation menu
    To create a reusable component for the minor navigation menu, i added a
    content folder with json files to describe my menu content.
    
    - add minor navigation menu component
    - add content file

---
+++ src/components/MinorNavigationMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <a v-for="item in menuItems" :key="item.name" class="d-block mb-1 pb-1 border-bottom w-100 text-dark no-decoration" :href="`/${item.url}`">{{ item.name }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    menuItems: Array
+  }
+}
+</script>
+
+<style>
+.no-decoration {
+  text-decoration: none;
+}
+</style>
